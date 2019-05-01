2198e3b89638fa41fa40c584327d461383f9e73c
cbwang2016 <ano>
2019-05-01 18:59:50 +0800

    add support for IBM

--- ibmqx4.jl
+++ ibmqx4.jl
@@ -3,12 +3,6 @@ using Yao, LinearAlgebra, YaoBlocks
 Ugate(nbit::Int, i::Int) = put(nbit, i=>chain(Rz(0), Rx(0), Rz(0)));
 Ugate(i::Int) = put(i=>chain(Rz(0), Rx(0), Rz(0)));
 
-function build_circuit()
-    # circuit = chain(2, control(1, 2=>H), Ugate(1))
-    circuit = chain(2, control(1, 2=>H), Ugate(1))
-    return circuit
-end
-
 struct InitStateMatrix
     m1::Matrix{ComplexF64}
     m2::Matrix{ComplexF64}
@@ -16,13 +10,13 @@ end
 
 function build_state(mats::InitStateMatrix)
     # return zero_state(2) |> chain(2, put(1=>chain(Rz(1), Rx(2))), put(2=>chain(Rz(1), Rx(2))))
-    return zero_state(2) |> chain(2, put(2, (1)=>matblock(mats.m1)), put(2, (2)=>matblock(mats.m2)))
+    return zero_state(3) |> chain(3, put(3, (2)=>matblock(mats.m1)), put(3, (3)=>matblock(mats.m2)))
 end
 
 # @show probs(build_state())
 
 function overlap(x::InitStateMatrix)
-    return dot(apply!(build_state(x), chain(2, control(1, 2=>X), put(1=>H))) |> probs,[1, 1, 1, -1])
+    return dot(apply!(build_state(x), chain(3, control(2, 3=>X), put(2=>H))) |> probs, [1, 1, 1, -1, 1, 1, 1, -1])
 end
 
 function cost(trainX, trainY, circuit, c)
@@ -58,13 +52,9 @@ end
 
 function generate_gates_set(n::Int64)
     rtn = []
-    for i in 1:n
-        for j in 1:n
-            if i != j
-                push!(rtn, control(n, i, j=>X))
-            end
-        end
-    end
+    push!(rtn, control(n, 2, 1=>X))
+    push!(rtn, control(n, 3, 1=>X))
+    push!(rtn, control(n, 3, 2=>X))
 
     for i in 1:n
         push!(rtn, Ugate(n, i))
@@ -116,7 +106,7 @@ end
 
 using Flux.Optimise
 
-c = [1, 1, 1, -1];
+c = [1, -1, 1, -1, 1, -1, 1, -1];
 # circuit = build_circuit()
 trainX = [InitStateMatrix(rand_unitary(2), rand_unitary(2)) for _ in 1:8]
 trainY = map(overlap, trainX)
@@ -131,7 +121,7 @@ function train(circuit)
         push!(history, now_cost)
         ps = parameters(circuit)
         # @show (i, ps, now_cost)
-        if abs(now_cost - last_cost) < 1e-8 || now_cost < 1e-7
+        if abs(now_cost - last_cost) < 1e-9 || now_cost < 1e-8
             # @show now_cost
             break
         end
@@ -142,12 +132,12 @@ function train(circuit)
 end
 
 function find_algo(annealing_param, d::Int64)
-    gate_set = generate_gates_set(2)
-    circuit = generate_init_circuit(2, d)
+    gate_set = generate_gates_set(3)
+    circuit = generate_init_circuit(3, d)
     (best_cost, history) = train(circuit)
     count = 0
     circuit_backup = circuit
-    while best_cost > 1e-7 && count < 1000
+    while best_cost > 1e-8 && count < 1000
         count += 1;
         circuit_backup = copy(circuit)
         circuit = random_modify(circuit, gate_set)
@@ -167,7 +157,21 @@ function find_algo(annealing_param, d::Int64)
     return circuit
 end
 
-@show find_algo(10.0, 2)
+function find_nearest_pi(x::Float64)
+    rtn = mod(x, 2*π);
+    rtn = rtn > π ? rtn - 2*π : rtn;
+    rtn = π / 4 * round(4 * rtn / π)
+end
+
+function enhance_circuit_to_pi(circuit)
+    params = parameters(circuit)
+    params = map(find_nearest_pi, params)
+    dispatch!(circuit, params)
+end
+
+@show circuit = find_algo(10.0, 9)
+enhance_circuit_to_pi(circuit)
+@show cost(trainX, trainY, circuit, c)
 # (now_cost, history) = train(circuit);
 
 # using Plots
